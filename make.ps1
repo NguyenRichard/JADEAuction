3181c53a38e4c3563d90c31be8b2023571f07ed4
Quentin Tran <quentin.tran@telecom-sudparis.eu>
2019-12-10 12:07:26 +0100

    Maj du makefile

--- make.ps1
+++ make.ps1
@@ -2,4 +2,5 @@ Write-Output "Compilation starts"
 cd ../../
 javac examples\JADEAuction\JINBuyerAgent.java
 javac .\examples\JADEAuction\JINSellerAgent.java
+cd examples\JADEAuction\
 Write-Output "FINISH !"
